7650c12c5944b15ef03232feec3699770b9ac2bb
Valkried <celebow@gmail.com>
2017-09-28 15:20:28 +0200

    Ajout des routes publiques dans le CoreController, ajout de chemins à base.html.twig, création des pages pour ces routes, petite correction CSS pour le header

--- src/BH3Bundle/Controller/CoreController.php
+++ src/BH3Bundle/Controller/CoreController.php
@@ -12,9 +12,65 @@ class CoreController extends Controller
      */
     public function indexAction()
     {
-        return $this->render('BH3Bundle:Public:index.html.twig', array(
-            // ...
+        return $this->render('BH3Bundle:Public:index.html.twig');
+    }
+
+    /**
+     * @Route("/presentation", name="presentation")
+     */
+    public function presentationAction()
+    {
+        return $this->render('BH3Bundle:Public:presentation.html.twig');
+    }
+
+    /**
+     * @Route("/staff", name="staff")
+     */
+    public function staffAction()
+    {
+        return $this->render('BH3Bundle:Public:staff.html.twig');
+    }
+
+    /**
+     * @Route("/palmares", name="palmares")
+     */
+    public function palmaresAction()
+    {
+        return $this->render('BH3Bundle:Public:palmares.html.twig');
+    }
+
+    /**
+     * @Route("/roster/{id}", name="roster")
+     */
+    public function rosterAction($id)
+    {
+        return $this->render('BH3Bundle:Public:roster.html.twig', array(
+            'roster' => $id
         ));
     }
 
+    /**
+     * @Route("/partenaires", name="partenaires")
+     */
+    public function partenairesAction()
+    {
+        return $this->render('BH3Bundle:Public:partenaires.html.twig');
+    }
+
+    /**
+     * @Route("/livestream", name="livestream")
+     */
+    public function livestreamAction()
+    {
+        return $this->render('BH3Bundle:Public:livestream.html.twig');
+    }
+
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contactAction()
+    {
+        return $this->render('BH3Bundle:Public:contact.html.twig');
+        
+    }
 }

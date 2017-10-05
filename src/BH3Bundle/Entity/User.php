4e5b3d850c1e4dc0f01debcfb8753cccb3535f76
Valkried <celebow@gmail.com>
2017-10-05 17:12:09 +0200

    Ajout et paramétrage de FOSUserBundle

---
+++ src/BH3Bundle/Entity/User.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace BH3Bundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="bh3_users")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
